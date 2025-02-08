b9e6b62a2b6c2d9071fc9b5740595609ce734f03
spraws <145443721+spraws@users.noreply.github.com>
2025-02-08 18:10:06 +0000

    updated about

--- PortfolioV2/src/assets/about/AboutContent.jsx
+++ PortfolioV2/src/assets/about/AboutContent.jsx
@@ -1,4 +1,8 @@
 import "./about.css";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGolang} from "@fortawesome/free-brands-svg-icons";
+import {faPython} from "@fortawesome/free-brands-svg-icons";
+
 
 function AboutContent() {
     return (
@@ -10,7 +14,15 @@ function AboutContent() {
                 </p>
                 <p className="sect-2">
                     Forensics is my main field of interest, but I am also interested in neural networks, machine learning and offensive security.
-                </p>    
+                </p>
+                <p className="sect-3">
+                    Currently I am learning <a href="https://go.dev/"><FontAwesomeIcon icon={faGolang} className="goLang" /></a>  and Python <a href="https://www.python.org/"><FontAwesomeIcon icon={faPython} className="python" /></a>
+                    <br />
+
+                    On the project page, you can find some of the projects I have worked on.
+                
+                </p>        
+                
             </div>
         </div>
     ) 
