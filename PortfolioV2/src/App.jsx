293774e6ae1435f2dd5da7af79ea45c372807e54
spraws <145443721+spraws@users.noreply.github.com>
2025-02-08 00:40:43 +0000

    Add Font Awesome icons to project; update package.json and package-lock.json

--- PortfolioV2/src/App.jsx
+++ PortfolioV2/src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import AboutContent from './assets/about/AboutContent';
 import Nav from './assets/nav/nav';
 import Header from './assets/header/Header'
-import "@fontsource/source-serif-pro/400.css"; // Specify weight
 function App() {
   
   return(
@@ -10,8 +9,11 @@ function App() {
       <Nav />
       <Header />
       <AboutContent />
+
     </div>
   );
 }
+
+
 
 export default App
